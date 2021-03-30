c674e2dd5100a6a1bf43ff8b75e2f35025719183
Junaid Khan <junaidk7998@gmail.com>
2021-03-30 09:35:04 -0700

    Add files via upload

---
+++ TicTacToe.cpp
@@ -0,0 +1,9 @@
+// a tic tac toe game
+#include <iostream>
+#include "play.cpp"
+
+int main() {
+	introduction();
+	take_turn();
+	end_game();
+}
---
+++ play.h
@@ -0,0 +1,10 @@
+#pragma once
+bool is_winner();
+bool filled_up();
+void introduction();
+void take_turn();
+void set_position();
+void update_board();
+void change_player();
+void draw();
+void end_game();
